35da54445d1e300a1c26ad690433055c388b42af
David Pérez-Suárez <d.perez-suarez@ucl.ac.uk>
2025-10-27 10:36:11 +0000

    Write unit tests

--- test_times.py
+++ test_times.py
@@ -1,7 +1,42 @@
 from times import compute_overlap_time, time_range
 
+
 def test_generic_case():
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
-    expected = [("2010-01-12 10:30:00","2010-01-12 10:37:00"), ("2010-01-12 10:38:00", "2010-01-12 10:45:00")]
+    expected = [
+        ("2010-01-12 10:30:00", "2010-01-12 10:37:00"),
+        ("2010-01-12 10:38:00", "2010-01-12 10:45:00"),
+    ]
     assert compute_overlap_time(large, short) == expected
+
+
+def test_ranges_dont_overlap():
+    first = time_range("2010-01-12 10:00:00", "2010-01-12 10:30:00")
+    second = time_range("2010-01-12 11:00:00", "2010-01-12 11:30:00")
+    expected = []
+
+    assert compute_overlap_time(first, second) == expected
+
+
+def test_ranges_touching():
+    first = time_range("2010-01-12 10:00:00", "2010-01-12 11:00:00")
+    second = time_range("2010-01-12 11:00:00", "2010-01-12 12:00:00")
+    expected = [("2010-01-12 11:00:00", "2010-01-12 11:00:00")]
+
+    assert compute_overlap_time(first, second) == expected
+
+
+def test_ranges_multiple_intervals():
+    first = time_range("2010-01-12 10:00:00", "2010-01-12 11:00:00", 4)
+    second = time_range(
+        "2010-01-12 10:00:00", "2010-01-12 12:00:00", 4, gap_between_intervals_s=60
+    )
+    expected = [
+        ("2010-01-12 10:00:00", "2010-01-12 10:15:00"),
+        ("2010-01-12 10:15:00", "2010-01-12 10:29:15"),
+        ("2010-01-12 10:30:15", "2010-01-12 10:45:00"),
+        ("2010-01-12 10:45:00", "2010-01-12 10:59:30"),
+    ]
+
+    assert compute_overlap_time(first, second) == expected
